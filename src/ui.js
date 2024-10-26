50f2df44005ab1530268c4b6c0cad46fc1022371
flighty <vrtxrry@pm.me>
2024-10-26 08:35:09 +0300

    Add feature to set created planet size

--- src/ui.js
+++ src/ui.js
@@ -1,8 +1,13 @@
 
+const MIN_PLANET_MASS = 10;
+const DEFAULT_PLANET_MASS = 100;
+
+let selectedPlanetMass = DEFAULT_PLANET_MASS;
 let mousePressStart = new p5.Vector();
 
 function mousePressed(){
 
+	selectedPlanetMass = DEFAULT_PLANET_MASS;
 	mousePressStart.set(mouseX, mouseY);
 }
 
@@ -10,12 +15,23 @@ function mouseReleased(){
 
 	let velocity = p5.Vector.sub(mousePressStart, new p5.Vector(mouseX, mouseY));
 	velocity.div(50);
-    planets.push(new Planet(100, mousePressStart.copy(), velocity));
+    planets.push(new Planet(selectedPlanetMass, mousePressStart.copy(), velocity));
+}
+
+function mouseWheel(event){
+
+	selectedPlanetMass += event.delta * 20;
+	selectedPlanetMass = max(selectedPlanetMass, MIN_PLANET_MASS);
 }
 
 function drawUi(){
 
 	if(!mouseIsPressed) return;
+
+	fill(150);
+	stroke(150);
+	let selectedPlanetDiameter = Planet.getRadius(selectedPlanetMass) * 2;
+	ellipse(mousePressStart.x, mousePressStart.y, selectedPlanetDiameter);
 
 	stroke(255);
 	line(mousePressStart.x, mousePressStart.y, mouseX, mouseY);
