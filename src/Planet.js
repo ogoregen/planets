852244fa358db938ec4bfc578d18ce69a300cc9b
ogoregen <37447279+ogoregen@users.noreply.github.com>
2024-10-30 17:53:40 +0300

    Refactor Planet.attract()

--- src/Planet.js
+++ src/Planet.js
@@ -37,10 +37,11 @@ class Planet extends Body{
 		
 		if(planet1 == planet2) return;
 		
-		let distance = p5.Vector.dist(planet1.position, planet2.position);
+		let direction = p5.Vector.sub(planet1.position, planet2.position);
+		let distance = direction.mag();
 		let magnitude = GRAVITATIONAL_CONSTANT * planet1.mass * planet2.mass / Math.pow(distance, 2);
 		
-		let force = p5.Vector.sub(planet1.position, planet2.position);
+		let force = direction;
 		force.setMag(magnitude);
 		
 		planet2.applyForce(force);
